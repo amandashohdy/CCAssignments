14829dc31275a0b41dc4b2a29d2c274d4e5f2712
amandashohdy <116974156+amandashohdy@users.noreply.github.com>
2023-05-08 15:43:21 -0700

    Update Programmer.cpp

--- CppProjects/CS1C/HW03/Programmer.cpp
+++ CppProjects/CS1C/HW03/Programmer.cpp
@@ -1,9 +1,9 @@
 /*********************************
  * AUTHOR       : Amanda Shohdy
- * ASSIGNMENT   : HW02
+ * ASSIGNMENT   : HW03
  * CLASS        : CS1C
  * SECTION      : TTH 1:30-3:50p
- * DUE DATE     : 26 January 2023
+ * DUE DATE     : 9 February 2023
 **********************************/
 
 #include "Programmer.h"
