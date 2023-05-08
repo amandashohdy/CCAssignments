340c183bdf03dfa07091a22fcf8c81a43f10c7b8
amandashohdy <116974156+amandashohdy@users.noreply.github.com>
2023-05-08 15:57:27 -0700

    Create Date.cpp

---
+++ CppProjects/CS1C/HW03_Class_Constructors_Inheritance_&_Init/Date.cpp
@@ -0,0 +1,56 @@
+/*********************************
+ * AUTHOR       : Amanda Shohdy
+ * ASSIGNMENT   : HW03
+ * CLASS        : CS1C
+ * SECTION      : TTH 1:30-3:50p
+ * DUE DATE     : 9 February 2023
+**********************************/
+
+#include "Date.h" 
+#include <string>
+#include <iostream>
+using namespace std ;
+
+/************************************************
+ * Method Date(): Class Date
+ * 		This method is a constructor that 
+ * initializes the year, month, and day members.
+ * 
+ * PRE-CONDITIONS
+ * 		none
+ * POST-CONDITIONS
+ * 		The members year, month, and day are 
+ * initialized to their default values.
+ ************************************************/
+Date::Date()
+: year{9999}, month{1}, day{1} { }
+
+/************************************************
+ * Method Date(): Class Date
+ * 		This method is a constructor that 
+ * initializes the year, month, and day members.
+ * 
+ * PRE-CONDITIONS
+ * 		int mm - month of the date
+ * 		int dd - day of the date
+ * 		int yy - year of the date
+ * POST-CONDITIONS
+ * 		The members year, month, and day are 
+ * initialized to their passed values.
+ ************************************************/
+Date::Date(int mm, int dd, int yy) 
+: year{yy}, month{mm}, day{dd} { }
+
+/************************************************
+ * Method PrintDate(): Class Date
+ * 		This method outputs the date.
+ * 
+ * PRE-CONDITIONS
+ * 		none
+ * POST-CONDITIONS
+ * 		The date is output to the console.
+ ************************************************/
+void Date::PrintDate() const
+{
+	cout << (month) << "/" << day << "/" << year ;
+}
