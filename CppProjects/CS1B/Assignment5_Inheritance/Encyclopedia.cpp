e8b1b06b1128406842c72ef67ef07c7cb490e3d9
amandashohdy <116974156+amandashohdy@users.noreply.github.com>
2023-05-08 19:29:12 -0700

    Add files via upload

---
+++ CppProjects/CS1B/Assignment5_Inheritance/Encyclopedia.cpp
@@ -0,0 +1,88 @@
+/*********************************
+ * AUTHOR       : Amanda Shohdy
+ * ASSIGNMENT   : Assignment 5
+ * CLASS        : CS1B
+ * SECTION      : MW 1:30 - 3:50p
+ * DUE DATE     : 7 November 2022
+ **********************************/
+
+#include "Encyclopedia.h"
+#include <string>
+#include <iostream>
+using namespace std ;
+
+/***************************************************************************
+ * Method SetEdition: Class Encyclopedia
+ *      This method sets the edition attribute of the encyclopedia class
+ *
+ * PRE-CONDITIONS
+ *      userEdition     : edition input by the user
+ * POST-CONDITIONS
+ *      edition attribute is assigned to the value of userEdition. There
+ *      is no return value.
+ ***************************************************************************/
+void Encyclopedia::SetEdition(string userEdition)
+{
+    edition = userEdition ;
+}
+
+/***************************************************************************
+ * Method SetNumVolumes: Class Encyclopedia
+ *      This method sets the numVolumes attribute of the encyclopedia class
+ *
+ * PRE-CONDITIONS
+ *      userVolumes     : number of volumes input by the user
+ * POST-CONDITIONS
+ *      numVolumes attribute is assigned to the value of userVolumes. There
+ *      is no return value.
+ ***************************************************************************/
+void Encyclopedia::SetNumVolumes(int userVolumes)
+{
+    numVolumes = userVolumes ;
+}
+
+/***************************************************************************
+ * Method GetEdition: Class Encyclopedia
+ *      This method returns the edition attribute.
+ *
+ * PRE-CONDITIONS
+ *      none
+ * POST-CONDITIONS
+ *      edition attribute is returned.
+ ***************************************************************************/
+string Encyclopedia::GetEdition() const
+{
+    return edition ;
+}
+
+/***************************************************************************
+ * Method GetNumVolumes: Class Encyclopedia
+ *      This method returns the numVolumes attribute.
+ *
+ * PRE-CONDITIONS
+ *      none
+ * POST-CONDITIONS
+ *      numVolumes attribute is returned.
+ ***************************************************************************/
+int Encyclopedia::GetNumVolumes() const
+{
+    return numVolumes ;
+}
+
+/***************************************************************************
+ * Method PrintInfo: Class Encyclopedia
+ *      This method outputs the information of an encyclopedia object
+ *
+ * PRE-CONDITIONS
+ *      none
+ * POST-CONDITIONS
+ *      The information of an encyclopedia object is output.
+ ***************************************************************************/
+void Encyclopedia::PrintInfo()
+{
+    Book::PrintInfo() ;
+    cout << "   Edition: " << edition << endl ;
+    cout << "   Number of Volumes: " << numVolumes << endl ;
+}
+
+
---
+++ CppProjects/CS1B/Assignment5_Inheritance/Encyclopedia.h
@@ -0,0 +1,98 @@
+/*********************************
+ * AUTHOR       : Amanda Shohdy
+ * ASSIGNMENT   : Assignment 5
+ * CLASS        : CS1B
+ * SECTION      : MW 1:30 - 3:50p
+ * DUE DATE     : 7 November 2022
+ **********************************/
+
+#ifndef ENCYCLOPEDIA_H_
+#define ENCYCLOPEDIA_H_
+
+#include "Book.h"
+#include <string>
+using namespace std ;
+
+class Encyclopedia : public Book
+{
+    public :
+
+        /**************
+         ** MUTATORS **
+         **************/
+        void SetEdition(string userEdition) ;
+        void SetNumVolumes(int userVolumes) ;
+
+        /***************
+         ** ACCESSORS **
+         ***************/
+        string GetEdition() const ;
+        int GetNumVolumes() const ;
+
+        void PrintInfo() ;
+
+    private :
+        string edition ;            // PROCESSING - encyclopedia edition
+        int numVolumes ;            // PROCESSING - number of volumes
+} ;
+
+
+/***************************************************************************
+ * Encyclopedia Class
+ *      This class is derived from the Book class. This class represents
+ *      the encyclopedia object. It manages 2 attributes: edition and
+ *      numVolumes.
+ ***************************************************************************/
+
+
+/**************
+ ** MUTATORS **
+ **************/
+
+/***************************************************************************
+ * void SetEdition(string userEdition) ;
+ *      Mutator	  :     Sets the edition attribute to the value input by
+ *                      the user
+ *      Parameter :     userEdition (string)  // IN - encyclopedia edition
+ *      Return    :     none
+ ***************************************************************************/
+
+/***************************************************************************
+ * void SetNumVolumes(int userVolumes) ;
+ *      Mutator   :     Sets the number of volumes attribute to the value
+ *                      input by the user
+ *      Parameter :     userVolumes (int)  // IN - number of volumes
+ *      Return    :     none
+ ***************************************************************************/
+
+
+/***************
+ ** ACCESSORS **
+ ***************/
+
+/***************************************************************************
+ * string GetEdition() const ;
+ *      Accessor  :     Returns the edition of the encyclopedia
+ *
+ *      Parameter :     none
+ *      Return    :     edition (string) // PROCESSING - encyclopedia edition
+ ***************************************************************************/
+
+/***************************************************************************
+ * int GetNumVolumes() const ;
+ *      Accessor  : 		Returns the number of volumes
+ *
+ *      Parameter :     none
+ *      Return    :     numVolumes (int) // PROCESSING - number of volumes
+ ***************************************************************************/
+
+/***************************************************************************
+ * void PrintInfo() ;
+ *      Accessor  :     Outputs the information of an encyclopedia object
+ *
+ *      Parameter :     none
+ *      Return    :     none
+ ***************************************************************************/
+
+#endif /* ENCYCLOPEDIA_H_ */
+
