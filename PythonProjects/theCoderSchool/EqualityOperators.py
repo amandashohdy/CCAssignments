3ce7f76b5427ade57bb1dbe09dbed8ca8f17c514
amandashohdy <116974156+amandashohdy@users.noreply.github.com>
2023-08-02 14:48:47 -0700

    Create EqualityOperators.py

---
+++ PythonProjects/theCoderSchool/EqualityOperators.py
@@ -0,0 +1,8 @@
+num1 = int(input("Pick any number    : "))
+num2 = int(input("Pick another number: "))
+print(str(num1), "is greater than", str(num2), "... ", (num1>num2))
+print(str(num1), "is less    than", str(num2), "... ", (num1<num2))
+print(str(num1), "is equal   to  ", str(num2), "... ", (num1==num2))
+
+# to learn equality operators and the
+# boolean data type.
