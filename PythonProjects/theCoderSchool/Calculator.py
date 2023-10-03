0ea1bb8e9d6b1672f448b1887e24cccede4373ed
amandashohdy <116974156+amandashohdy@users.noreply.github.com>
2023-10-02 22:49:06 -0700

    Delete PythonProject directory

--- PythonProjects/theCoderSchool/Calculator.py
+++
@@ -1,3 +0,0 @@
-value1 = float(input("Enter your first number: "))
-value2 = float(input("Enter your second number: "))
-print(value1, "+", value2, "=", value1+value2)
--- PythonProjects/theCoderSchool/Single Digit Guesser.py
+++
@@ -1,8 +0,0 @@
-correctNumber = 5
-userGuess = int(input("Guess the number: "))
-if userGuess == correctNumber :
-  print("Correct")
-elif userGuess < correctNumber :
-  print("Your guess is less than the number.")
-elif userGuess > correctNumber :
-  print("Your guess is greater than the number.")
