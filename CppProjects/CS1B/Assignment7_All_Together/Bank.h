5d8e6fed7514c087aa3f78e13925984b870f0315
amandashohdy <116974156+amandashohdy@users.noreply.github.com>
2023-10-03 00:41:45 -0700

    Update Bank.h

--- CppProjects/CS1B/Assignment7_All_Together/Bank.h
+++ CppProjects/CS1B/Assignment7_All_Together/Bank.h
@@ -1,9 +1,9 @@
 /*********************************
- * AUTHOR		: AMANDA SHOHDY
+ * AUTHOR	: AMANDA SHOHDY
  * ASSIGNMENT	: ASSIGNMENT 7
- * CLASS			: CS1B
- * SECTION		: MW 1:30 - 3:50p
- * DUE DATE		: 12 DECEMBER 2022
+ * CLASS	: CS1B
+ * SECTION	: MW 1:30 - 3:50p
+ * DUE DATE	: 12 DECEMBER 2022
  **********************************/
 
 #ifndef BANK_H_
@@ -63,45 +63,45 @@ class Bank
  * 		Mutator 	  :	Creates an object of the specified class and adds a pointer to the
  * 						   object to the accounts list.
  *
- * 		Parameter  : 	date (Date)			// IN - openings date
- * 						   number (int)		// IN - account number
- * 						   amount (float)		// IN - amount of money being deposited
- * 					   	name (string)		// IN - account holder's name
- * 						   type (string)		// IN - type of account being opened
+ * 		Parameter  : 	date (Date)		// IN - openings date
+ *                              number (int)		// IN - account number
+ *                              amount (float)		// IN - amount of money being deposited
+ *                              name (string)		// IN - account holder's name
+ *                              type (string)		// IN - type of account being opened
  * 		Return    :		none
  ************************************************************************************************/
 
 /************************************************************************************************
  * void Deposit(int accNum, Date date, float amount) ;
- * 		Mutator 	  :	finds the account being accessed in the accounts list based on the
+ * 		Mutator :	finds the account being accessed in the accounts list based on the
  * 						   account number and calls the respective accounts Deposit() method.
  *
- * 		Parameter  :	accNum (int)			// IN - account number
- * 					   	date (Date) 			// IN - transaction date
- * 						   amount (float)		   // IN - amount deposited
- * 		Return	  :	none
+ * 		Parameter:	accNum (int)        // IN - account number
+ *                              date (Date)         // IN - transaction date
+ *                              amount (float)	    // IN - amount deposited
+ * 		Return :	none
  ************************************************************************************************/
 
 /************************************************************************************************
  * void Withdraw(int accNum, Date date, float amount) ;
- *  		Mutator 	  :	finds the account being accessed in the accounts list based on the
+ *  		Mutator:	finds the account being accessed in the accounts list based on the
  *  						   account number and calls the respective accounts Withdrawal() method.
  *
- * 		Parameter  :	accNum (int) 		// IN - account number
- * 						   date (date)			// IN - transaction date
- * 						   amount (float)		// IN - amount withdrawn
- * 		Return	  :	none
+ * 		Parameter:	accNum (int)       // IN - account number
+ *                              date (date)	   // IN - transaction date
+ *                              amount (float)	   // IN - amount withdrawn
+ * 		Return: 	none
  ************************************************************************************************/
 
 /************************************************************************************************
  * void Transfer(int accNum, Date date, float amount, int transfer) ;
- *   	Mutator 	  :	finds the two accounts being accessed in the accounts list based on
+ *   	        Mutator :	finds the two accounts being accessed in the accounts list based on
  *   				   	their accounts numbers and calls the TransferFunds() method.
  *
- * 		Parameter :		accNum (int) 		// IN - account number money is being transferred to
- * 						   date (Date) 		// IN - transaction date
- * 						   amount (float)		// IN - amount transferred
- * 					   	transfer (int) 	// IN - account number money is being transferred from
+ * 		Parameter :	accNum (int)    // IN - account number money is being transferred to
+ *                              date (Date) 	// IN - transaction date
+ *                              amount (float)	// IN - amount transferred
+ *                              transfer (int) 	// IN - account number money is being transferred from
  * 		Return	  :	none
  ************************************************************************************************/
 
@@ -112,27 +112,27 @@ class Bank
 
 /************************************************************************************************
  * string GetName(int accNumber) ;
- * 		Accessor  :		accesses the account holder's name based on the account number
+ * 		Accessor  :    accesses the account holder's name based on the account number
  *
- * 		Parameter :		accNumber (int) 						// IN - account number
- * 		Return	 :		current account's name (string) 	// OUT - account holder's name
+ * 		Parameter :    accNumber (int)                     // IN - account number
+ * 		Return	  :    current account's name (string)     // OUT - account holder's name
  ************************************************************************************************/
 
 /************************************************************************************************
  * float GetBalance(int accNumber) ;
- * 		Accessor  :		accesses the account's balance based on the account number
+ * 		Accessor  :    accesses the account's balance based on the account number
  *
- * 		Parameter :		accNumber (int)						   // IN - account number
- * 		Return	 :		current account's balance (float)	// OUT - account balance
+ * 		Parameter :    accNumber (int)                      // IN - account number
+ * 		Return	  :    current account's balance (float)    // OUT - account balance
  ************************************************************************************************/
 
 /************************************************************************************************
  * Account* Find(int accNumber) ;
- *		   Accessor  :		helper function to search through the accounts list and returnt the
- *						      pointer to a specified account
+ *		Accessor  :    helper function to search through the accounts list and returnt the
+ *                             pointer to a specified account
  *
- * 		Parameter :		accNumber (int) 			// IN - account number
- * 		Return	 :		currAccount (Account*)	// OUT - pointer to account object being accessed
+ * 		Parameter :    accNumber (int) 	        // IN - account number
+ * 		Return	  :    currAccount (Account*)	// OUT - pointer to account object being accessed
  ************************************************************************************************/
 
 #endif /* BANK_H_ */
