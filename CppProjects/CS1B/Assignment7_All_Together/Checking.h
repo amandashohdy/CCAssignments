6624b0409d86f7a1a18191a7d518e2d94d064245
amandashohdy <116974156+amandashohdy@users.noreply.github.com>
2023-10-03 00:57:36 -0700

    Update Checking.h

--- CppProjects/CS1B/Assignment7_All_Together/Checking.h
+++ CppProjects/CS1B/Assignment7_All_Together/Checking.h
@@ -1,9 +1,9 @@
 /*********************************
- * AUTHOR		: AMANDA SHOHDY
- * ASSIGNMENT	: ASSIGNMENT 7
- * CLASS			: CS1B
- * SECTION		: MW 1:30 - 3:50p
- * DUE DATE		: 12 DECEMBER 2022
+ * AUTHOR     : AMANDA SHOHDY
+ * ASSIGNMENT : ASSIGNMENT 7
+ * CLASS      : CS1B
+ * SECTION    : MW 1:30 - 3:50p
+ * DUE DATE   : 12 DECEMBER 2022
  **********************************/
 
 #ifndef CHECKING_H_
@@ -16,7 +16,6 @@
 class Checking : public Account
 {
 	public :
-
 		/******************
 		 ** CONSTRUCTORS **
 		 ******************/
@@ -28,16 +27,16 @@ class Checking : public Account
 		 **************/
 		void Withdrawal(Date today, float amount) override ;
 	private :
-		float overDraft ;		// PROCESSING - overDraw limit
-		float overFee ;			// PROCESSING - overDraw fee
+		float overDraft ;     // PROCESSING - overDraw limit
+		float overFee ;       // PROCESSING - overDraw fee
 } ;
 
 /*************************************************************************************************
  * Checking Class : Inherited from Account Class
- * 		This class represents the Checking object. It manages 5 attributes inherited
- * 		from the Account Class: clientName, acctNumber, currentBalance, openDate, and
- * 		lastAccessDate. It has constants representing the overdraw limit and the overdraw
- * 		fee.
+ *     This class represents the Checking object. It manages 5 attributes inherited
+ *     from the Account Class: clientName, acctNumber, currentBalance, openDate, and
+ *     lastAccessDate. It has constants representing the overdraw limit and the overdraw
+ *     fee.
  *************************************************************************************************/
 
 
@@ -47,23 +46,23 @@ class Checking : public Account
 
 /*************************************************************************************************
  * Checking() ;
- * 		Constructor :	Initialize class attributes to defaults
+ *     Constructor :     Initialize class attributes to defaults
  *
- * 		Parameter	:	none
- * 		Return		:	none
+ *     Parameter   :     none
+ *     Return      :     none
  *************************************************************************************************/
 
 /*************************************************************************************************
  * Checking(Date openingDate, string name, int accountNum, float balance, float over, float fee) ;
- *		   Constructor :	Initialize class attributes to passed values
+ *     Constructor :     Initialize class attributes to passed values
  *
- * 		Parameter   :	openingDate (Date)	// IN - date account was opened
- * 						   name (string)	   	// IN - name of account holder
- * 						   accountNum (int)		// IN - account number
- * 						   balance (float)		// IN - amount being deposited
- * 						   over (float)			// IN - overdraft limit
- * 						   fee (float)		   	// IN - overdraft fee
- * 		Return		:	none
+ *     Parameter   :     openingDate (Date)     // IN - date account was opened
+ *                       name (string)          // IN - name of account holder
+ *                       accountNum (int)       // IN - account number
+ *                       balance (float)        // IN - amount being deposited
+ *                       over (float)           // IN - overdraft limit
+ *                       fee (float)            // IN - overdraft fee
+ *     Return      :     none
  *************************************************************************************************/
 
 
@@ -73,12 +72,12 @@ class Checking : public Account
 
 /*************************************************************************************************
  * void Withdrawal(Date today, float amount) override ;
- * 		Mutator   :		Overrides the Withdrawal function of the Account class. Handles overdraft
- * 						   limits and charges fees for a negative balance.
+ *     Mutator   :     Overrides the Withdrawal function of the Account class. Handles overdraft
+ *                     limits and charges fees for a negative balance.
  *
- * 		Parameter :		today (Date)		// IN - transaction date
- * 					   	amount (float)	   // IN - amount being withdrawn
- * 		Return	 :		none
+ *     Parameter :     today (Date)      // IN - transaction date
+ *                     amount (float)    // IN - amount being withdrawn
+ *     Return    :     none
  *************************************************************************************************/
 
 
